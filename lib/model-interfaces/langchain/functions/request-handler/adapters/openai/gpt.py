b9ce2b8c9ce6309f6306ca00db18b570a0506c4e
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-09-05 09:12:48 -0700

    v0 litellm

--- lib/model-interfaces/langchain/functions/request-handler/adapters/openai/gpt.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/openai/gpt.py
@@ -1,6 +1,6 @@
 import os
 
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatLiteLLM
 
 from ..base import ModelAdapter
 from ..registry import registry
@@ -15,8 +15,8 @@ class GPTAdapter(ModelAdapter):
     def get_llm(self, model_kwargs={}):
         if not os.environ.get("OPENAI_API_KEY"):
             raise Exception("OPENAI_API_KEY must be set in the environment")
-
-        return ChatOpenAI(model_name=self.model_id, temperature=0, **model_kwargs)
+        # use 100+ LLMs simply pass in the LLM as model_name
+        return ChatLiteLLM(model_name=self.model_id, temperature=0, **model_kwargs)
 
 
 # Register the adapter
