3b3686a4ad992eea1e6ba3330c4f0f87d1cf66b8
Massimiliano Angelino <angmas@amazon.com>
2023-09-28 09:47:44 +0200

    feat: possibility to override the Retrieve prompts
    RAG uses langchain `ConversationalRetrievalChain` and with this change
    we allow to override the 2 prompts used by this chain
    this fixes the issue with the specific Claude formatting
    but also allows for further prompt tuning if necessary

--- lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/ai21_j2.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/ai21_j2.py
@@ -33,12 +33,12 @@ class AI21J2Adapter(ModelAdapter):
     def get_prompt(self):
         template = """Human: The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-        Current conversation:
-        {chat_history}
+Current conversation:
+{chat_history}
 
-        Question: {input}
-        
-        Assistant:"""
+Question: {input}
+
+Assistant:"""
 
         input_variables = ["input", "chat_history"]
         prompt_template_args = {
--- lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/claude.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/claude.py
@@ -1,8 +1,7 @@
 import genai_core.clients
 
+from langchain.llms import Bedrock
 from langchain.prompts.prompt import PromptTemplate
-
-from langchain.chat_models import BedrockChat
 
 from ..base import ModelAdapter
 from ..registry import registry
@@ -25,23 +24,40 @@ class BedrockClaudeAdapter(ModelAdapter):
         if "maxTokens" in model_kwargs:
             params["max_tokens_to_sample"] = model_kwargs["maxTokens"]
 
-        return BedrockChat(
-            client=bedrock,
+        return Bedrock(
+            client=client,
             model_id=self.model_id,
             model_kwargs=params,
             streaming=model_kwargs.get("streaming", False),
             callbacks=[self.callback_handler],
         )
 
+    def get_qa_prompt(self):
+        template = """
+
+Human: Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{context}
+
+Question: {question}
+
+Assistant:"""
+
+        return PromptTemplate(
+                template=template, input_variables=["context", "question"]
+                )
+
     def get_prompt(self):
-        template = """Human: The following is a friendly conversation between a human and an AI. If the AI does not know the answer to a question, it truthfully says it does not know.
+        template = """
 
-        Current conversation:
-        {chat_history}
+Human: The following is a friendly conversation between a human and an AI. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-        Question: {input}
-        
-        Assistant:"""
+Current conversation:
+{chat_history}
+
+Question: {input}
+
+Assistant:"""
 
         input_variables = ["input", "chat_history"]
         prompt_template_args = {
@@ -52,6 +68,21 @@ class BedrockClaudeAdapter(ModelAdapter):
         prompt_template = PromptTemplate(**prompt_template_args)
 
         return prompt_template
+
+    def get_condense_question_prompt(self):
+        template = """
+Human: Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+
+Assistant:"""
+
+        return PromptTemplate(input_variables=["chat_history", "question"],
+                            chat_history="{chat_history}",
+                            template=template)
+
 
 
 # Register the adapter
