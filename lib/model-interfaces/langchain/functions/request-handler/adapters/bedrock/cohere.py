4d274cc52182bfce1fc1ec9b4577e7a7f9b1774d
Massimiliano Angelino <38036163+massi-ang@users.noreply.github.com>
2023-09-28 17:49:18 +0200

    Feat cohere support (#61)
    * feat: possibility to override the Retrieve prompts
    
    RAG uses langchain `ConversationalRetrievalChain` and with this change
    we allow to override the 2 prompts used by this chain
    this fixes the issue with the specific Claude formatting
    but also allows for further prompt tuning if necessary
    
    * fix: var name
    
    * feat: adding cohere model (#58)
    
    * fix: expose classes in __init__.py

---
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/cohere.py
@@ -0,0 +1,57 @@
+import genai_core.clients
+
+from langchain.llms import Bedrock
+from langchain.prompts.prompt import PromptTemplate
+
+from ..base import ModelAdapter
+from ..registry import registry
+
+
+class BedrockCohereCommandAdapter(ModelAdapter):
+    def __init__(self, model_id, *args, **kwargs):
+        self.model_id = model_id
+
+        super().__init__(*args, **kwargs)
+
+    def get_llm(self, model_kwargs={}):
+        bedrock = genai_core.clients.get_bedrock_client()
+
+        params = {}
+        if "temperature" in model_kwargs:
+            params["temperature"] = model_kwargs["temperature"]
+        if "maxTokens" in model_kwargs:
+            params["max_tokens"] = model_kwargs["maxTokens"]
+        params["return_likelihood"] = "GENERATION"
+
+        return Bedrock(
+            client=bedrock,
+            model_id=self.model_id,
+            model_kwargs=params,
+            streaming=model_kwargs.get("streaming", False),
+            callbacks=[self.callback_handler],
+        )
+
+    def get_prompt(self):
+        template = """
+
+Human: The following is a friendly conversation between a human and an AI. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Current conversation:
+{chat_history}
+
+Question: {input}
+
+Assistant:"""
+
+        input_variables = ["input", "chat_history"]
+        prompt_template_args = {
+            "chat_history": "{chat_history}",
+            "input_variables": input_variables,
+            "template": template,
+        }
+        prompt_template = PromptTemplate(**prompt_template_args)
+
+        return prompt_template
+
+# Register the adapter
+registry.register(r"^bedrock.cohere.command-text*", BedrockCohereCommandAdapter)
