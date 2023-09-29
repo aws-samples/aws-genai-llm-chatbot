fe88f8346d797c40ad56de417b2cadcf5f017ec9
Sergey Pugachev <pugachev@amazon.com>
2023-09-29 14:32:27 +0100

    Container versions updated

--- lib/sagemaker-model/container-images.ts
+++ lib/sagemaker-model/container-images.ts
@@ -4,10 +4,10 @@ export class ContainerImages {
   HF_PYTORCH_INFERENCE
   https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/image_uri_config/huggingface.json
   */
-  static readonly HF_PYTORCH_INFERENCE_4_28_1 =
-    "huggingface-pytorch-inference:2.0.0-transformers4.28.1-gpu-py310-cu118-ubuntu20.04";
   static readonly HF_PYTORCH_INFERENCE_4_26_0 =
     "huggingface-pytorch-inference:1.13.1-transformers4.26.0-gpu-py39-cu117-ubuntu20.04";
+  static readonly HF_PYTORCH_INFERENCE_4_28_1 =
+    "huggingface-pytorch-inference:2.0.0-transformers4.28.1-gpu-py310-cu118-ubuntu20.04";
   static readonly HF_PYTORCH_INFERENCE_LATEST =
     ContainerImages.HF_PYTORCH_INFERENCE_4_28_1;
   /*
@@ -20,14 +20,18 @@ export class ContainerImages {
     "huggingface-pytorch-tgi-inference:2.0.0-tgi0.8.2-gpu-py39-cu118-ubuntu20.04";
   static readonly HF_PYTORCH_LLM_TGI_INFERENCE_0_9_3 =
     "huggingface-pytorch-tgi-inference:2.0.1-tgi0.9.3-gpu-py39-cu118-ubuntu20.04";
+  static readonly HF_PYTORCH_LLM_TGI_INFERENCE_1_0_3 =
+    "huggingface-pytorch-tgi-inference:2.0.1-tgi1.0.3-gpu-py39-cu118-ubuntu20.04";
   static readonly HF_PYTORCH_LLM_TGI_INFERENCE_LATEST =
-    ContainerImages.HF_PYTORCH_LLM_TGI_INFERENCE_0_9_3;
+    ContainerImages.HF_PYTORCH_LLM_TGI_INFERENCE_1_0_3;
   /*
   DJL_INFERENCE_DEEPSPEED
   https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/image_uri_config/djl-deepspeed.json
   */
   static readonly DJL_INFERENCE_DEEPSPEED_0_8_3 =
     "djl-inference:0.22.1-deepspeed0.8.3-cu118";
+  static readonly DJL_INFERENCE_DEEPSPEED_0_9_2 =
+    "djl-inference:0.22.1-deepspeed0.9.2-cu118";
   static readonly DJL_INFERENCE_DEEPSPEED_LATEST =
-    ContainerImages.DJL_INFERENCE_DEEPSPEED_0_8_3;
+    ContainerImages.DJL_INFERENCE_DEEPSPEED_0_9_2;
 }
