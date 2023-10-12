f0290f7b7a7909b194331d844c51206a2e679b0a
Donato Azevedo <donatoaz@gmail.com>
2023-10-12 14:59:05 -0300

    Applying comments from bigadsoleiman

--- lib/user-interface/react-app/vite.config.ts
+++ lib/user-interface/react-app/vite.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
           aws_user_pools_id: process.env.AWS_USER_POOLS_ID,
           aws_user_pools_web_client_id: process.env.AWS_USER_POOLS_WEB_CLIENT_ID,
           config: {
-            api_endpoint: `https://${process.env.API_DISTRIBUTION_NAME}/api`,
-            websocket_endpoint: `wss://${process.env.API_DISTRIBUTION_NAME}/socket`,
+            api_endpoint: `https://${process.env.API_DISTRIBUTION_DOMAIN_NAME}/api`,
+            websocket_endpoint: `wss://${process.env.API_DISTRIBUTION_DOMAIN_NAME}/socket`,
             rag_enabled: ['T', "t", "true", "True", "TRUE", "1"].includes(process.env.RAG_ENABLED),
             default_embeddings_model: process.env.DEFAULT_EMBEDDINGS_MODEL,
             default_cross_encoder_model: process.env.DEFAULT_CROSS_ENCODER_MODEL,
