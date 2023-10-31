e30ebc59854a2f1a5b2b6f9cefbf2fad4698918a
Massimiliano Angelino <angmas@amazon.com>
2023-10-31 19:58:28 +0100

    fix: remove unnecessary new lines in the Mistral prompts

--- lib/model-interfaces/langchain/functions/request-handler/adapters/sagemaker/mistralai/mistral_instruct.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/sagemaker/mistralai/mistral_instruct.py
@@ -60,9 +60,7 @@ class SMMistralInstructAdapter(ModelAdapter):
         )
     
     def get_qa_prompt(self):
-        template = """
-
-[INST] Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+        template = """[INST] Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 {context}
 
@@ -75,9 +73,7 @@ Question: {question}
         )
 
     def get_prompt(self):
-        template = """
-
-[INST] The following is a friendly conversation between a human and an AI. If the AI does not know the answer to a question, it truthfully says it does not know.
+        template = """[INST] The following is a friendly conversation between a human and an AI. If the AI does not know the answer to a question, it truthfully says it does not know.
 
 Current conversation:
 {chat_history}
@@ -97,8 +93,7 @@ Question: {input}
         return prompt_template
 
     def get_condense_question_prompt(self):
-        template = """
-{chat_history}
+        template = """{chat_history}
 
 [INST] Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
 Follow Up Input: {question}
