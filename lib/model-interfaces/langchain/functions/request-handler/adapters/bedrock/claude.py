cbfeb78cc0948d4d6df9a502cdab3c0182ddc97b
Riccardo Sartea <rsartea@amazon.com>
2023-11-07 16:14:41 +0000

    Changed get_condense_question_prompt for Claude adapter

--- lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/claude.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/claude.py
@@ -73,7 +73,7 @@ Assistant:"""
         template = """
 {chat_history}
 
-Human: Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+Human: Given the above conversation and a follow up input, rephrase the follow up input to be a standalone question, in the same language as the follow up input.
 Follow Up Input: {question}
 
 Assistant:"""
