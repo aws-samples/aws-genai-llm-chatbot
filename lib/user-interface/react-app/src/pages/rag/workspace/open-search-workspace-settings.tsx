43004cf007c36bffb909609b0f6d1d3f1e6acabf
cpollard0 <chris.pollard@rocketmail.com>
2023-12-01 15:56:47 -0500

    Update open-search-workspace-settings.tsx

--- lib/user-interface/react-app/src/pages/rag/workspace/open-search-workspace-settings.tsx
+++ lib/user-interface/react-app/src/pages/rag/workspace/open-search-workspace-settings.tsx
@@ -63,7 +63,7 @@ export default function OpenSearchWorkspaceSettings(
             <div>{props.workspace.embeddingsModelName}</div>
           </div>
           <div>
-            <Box variant="awsui-key-label">Embeddings dimentions</Box>
+            <Box variant="awsui-key-label">Embeddings dimensions</Box>
             <div>{props.workspace.embeddingsModelDimensions}</div>
           </div>
           <div>
