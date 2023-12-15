badc9acedc8ee352f492d4fd80513cf0dbf62475
Massimiliano Angelino <angmas@amazon.com>
2023-12-15 14:55:17 +0100

    feat(appsync): remove unused imports

--- lib/user-interface/index.ts
+++ lib/user-interface/index.ts
@@ -1,8 +1,6 @@
 import * as cognitoIdentityPool from "@aws-cdk/aws-cognito-identitypool-alpha";
 import * as cdk from "aws-cdk-lib";
 import * as cf from "aws-cdk-lib/aws-cloudfront";
-import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
-import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
