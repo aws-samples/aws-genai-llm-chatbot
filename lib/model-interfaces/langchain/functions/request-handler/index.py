53033d48fc14876f8017a28a98192ac20b1439d6
Massimiliano Angelino <angmas@amazon.com>
2024-03-05 14:13:05 +0100

    fix(chat): only send messages when token length > 0

--- lib/model-interfaces/langchain/functions/request-handler/index.py
+++ lib/model-interfaces/langchain/functions/request-handler/index.py
@@ -23,9 +23,9 @@ API_KEYS_SECRETS_ARN = os.environ["API_KEYS_SECRETS_ARN"]
 sequence_number = 0
 
 
-def on_llm_new_token(
-    user_id, session_id, self, token, run_id, *args, **kwargs
-):
+def on_llm_new_token(user_id, session_id, self, token, run_id, *args, **kwargs):
+    if token is None or len(token) == 0:
+        return
     global sequence_number
     sequence_number += 1
     run_id = str(run_id)
