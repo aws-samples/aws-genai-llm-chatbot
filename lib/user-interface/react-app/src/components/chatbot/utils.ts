b94f4f33cb068e4f735da954a871c27fb67d3c17
Massimiliano Angelino <angmas@amazon.com>
2024-03-05 14:11:53 +0100

    fix(chat): fixing chat for streamed content

--- lib/user-interface/react-app/src/components/chatbot/utils.ts
+++ lib/user-interface/react-app/src/components/chatbot/utils.ts
@@ -138,17 +138,7 @@ export function updateMessageHistoryRef(
       const lastMessage = messageHistory.at(-1)!;
       lastMessage.tokens = lastMessage.tokens ?? [];
       if (hasToken) {
-        // Workaround for text duplicates issue
-        if (
-          !lastMessage.tokens
-            .map((t) => t.sequenceNumber)
-            .includes(token.sequenceNumber)
-        ) {
-          lastMessage.tokens.push(token);
-        } else {
-          return;
-        }
-      } else {
+        lastMessage.tokens.push(token);
       }
 
       lastMessage.tokens.sort((a, b) => a.sequenceNumber - b.sequenceNumber);
@@ -175,14 +165,10 @@ export function updateMessageHistoryRef(
           tokens: lastMessage.tokens,
         };
       } else {
-        const contentFromTokens = lastMessage.tokens
-          .map((c) => c.value)
-          .join("");
-
         messageHistory[messageHistory.length - 1] = {
           ...lastMessage,
           type: ChatBotMessageType.AI,
-          content: contentFromTokens,
+          content: "",
           metadata,
           tokens: lastMessage.tokens,
         };
