3b9fa8c2f0927cb35a717f6b9236f73639859517
Massimiliano Angelino <angmas@amazon.com>
2024-03-07 08:31:14 +0100

    feat(mistral): support for mistral and mixtral models
    fixes to the LLama2 and mistral prompts

--- lib/model-interfaces/langchain/functions/request-handler/adapters/shared/meta/llama2_chat.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/shared/meta/llama2_chat.py
@@ -5,31 +5,25 @@ from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 
 
-Llama2ChatPrompt = """<<SYS>>
+Llama2ChatPrompt = """<s>[INST] <<SYS>>
 You are an helpful assistant that provides concise answers to user questions with as little sentences as possible and at maximum 3 sentences. You do not repeat yourself. You avoid bulleted list or emojis.
 <</SYS>>
 
-{chat_history}
+{chat_history}<s>[INST] Context: {input} [/INST]"""
 
-{input} [/INST]"""
-
-Llama2ChatQAPrompt = """<<SYS>>
+Llama2ChatQAPrompt = """<s>[INST] <<SYS>>
 Use the following conversation history and pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. You do not repeat yourself. You avoid bulleted list or emojis.
 <</SYS>>
 
-{chat_history}
-
-[INST] Context: {context} [/INST]
+{chat_history}<s>[INST] Context: {context}
 
 {question} [/INST]"""
 
-Llama2ChatCondensedQAPrompt = """<<SYS>>
+Llama2ChatCondensedQAPrompt = """<s>[INST] <<SYS>>
 Given the following conversation and the question at the end, rephrase the follow up input to be a standalone question, in the same language as the follow up input. You do not repeat yourself. You avoid bulleted list or emojis.
 <</SYS>>
 
-{chat_history}
-
-{question} [/INST]"""
+{chat_history}<s>[INST] {question} [/INST]"""
 
 
 Llama2ChatPromptTemplate = PromptTemplate.from_template(Llama2ChatPrompt)
@@ -53,7 +47,7 @@ class Llama2ConversationBufferMemory(ConversationBufferMemory):
                 if human_message_cnt == 0:
                     message = f"{m.content} [/INST]"
                 else:
-                    message = f"<s> [INST] {m.content} [/INST]"
+                    message = f"<s>[INST] {m.content} [/INST]"
                 human_message_cnt += 1
             elif isinstance(m, AIMessage):
                 message = f"{m.content} </s>"
