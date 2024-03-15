dc6a3e41549033ee3862b3fccb3e50f6d5b23889
Massimiliano Angelino <angmas@amazon.com>
2024-03-15 09:38:10 +0100

    feat(agents): keep first letter as avatar

--- lib/user-interface/react-app/src/components/chatbot/chat-message.tsx
+++ lib/user-interface/react-app/src/components/chatbot/chat-message.tsx
@@ -108,31 +108,9 @@ export default function ChatMessage(props: ChatMessageProps) {
           thumb === "up" ? props.onThumbsUp() : props.onThumbsDown();
         }}
         name={
-          props.message?.type === ChatBotMessageType.Human ? "Human" : undefined
-        }
-        avatarElement={
-          props.message?.type === ChatBotMessageType.AI ? (
-            <svg
-              viewBox="0 0 16 16"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  fill="#FFFFFF"
-                  fillRule="evenodd"
-                  d="M.5 2.75a2.25 2.25 0 114.28.97l1.345 1.344.284-.284a2.25 2.25 0 013.182 0l.284.284 1.344-1.344a2.25 2.25 0 111.06 1.06l-1.343 1.345.284.284a2.25 2.25 0 010 3.182l-.284.284 1.344 1.344a2.25 2.25 0 11-1.06 1.06l-1.345-1.343-.284.284a2.25 2.25 0 01-3.182 0l-.284-.284-1.344 1.344a2.25 2.25 0 11-1.06-1.06l1.343-1.345-.284-.284a2.25 2.25 0 010-3.182l.284-.284L3.72 4.781A2.25 2.25 0 01.5 2.75zM2.75 2a.75.75 0 100 1.5.75.75 0 000-1.5zm0 10.5a.75.75 0 100 1.5.75.75 0 000-1.5zm9.75.75a.75.75 0 111.5 0 .75.75 0 01-1.5 0zM13.25 2a.75.75 0 100 1.5.75.75 0 000-1.5zM7.47 5.841a.75.75 0 011.06 0L10.16 7.47a.75.75 0 010 1.06L8.53 10.16a.75.75 0 01-1.06 0L5.84 8.53a.75.75 0 010-1.06L7.47 5.84z"
-                  clipRule="evenodd"
-                ></path>
-              </g>
-            </svg>
-          ) : undefined
+          props.message?.type === ChatBotMessageType.Human
+            ? "Human"
+            : "Assistant"
         }
         expandableContent={
           props.message.type == ChatBotMessageType.AI &&
