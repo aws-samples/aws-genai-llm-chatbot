6d8b11cbfc0baa100c9de3643258e2ff503dde7e
Massimiliano Angelino <angmas@amazon.com>
2024-03-26 18:14:00 +0100

    feat(sessions): initial

--- lib/user-interface/react-app/src/components/chatbot/sessions.tsx
+++ lib/user-interface/react-app/src/components/chatbot/sessions.tsx
@@ -5,6 +5,7 @@ import {
   Pagination,
   Button,
   TableProps,
+  Header,
 } from "@cloudscape-design/components";
 import { DateTime } from "luxon";
 import { useState, useEffect, useContext, useCallback } from "react";
@@ -61,7 +62,7 @@ export default function Sessions(props: SessionsProps) {
 
   useEffect(() => {
     if (!appContext) return;
-    if (!props.toolsOpen) return;
+    //if (!props.toolsOpen) return;
 
     (async () => {
       setIsLoading(true);
@@ -92,100 +93,92 @@ export default function Sessions(props: SessionsProps) {
   };
 
   return (
-    <div style={{ padding: "0px 14px" }}>
-      <Table
-        {...collectionProps}
-        variant="embedded"
-        items={items}
-        pagination={<Pagination {...paginationProps} />}
-        loadingText="Loading history"
-        loading={isLoading}
-        resizableColumns
-        header={
-          <div style={{ paddingTop: "4px" }}>
-            <h2>History</h2>
-            <div>
-              <SpaceBetween direction="horizontal" size="m" alignItems="center">
-                <RouterButton
-                  iconName="add-plus"
-                  href={`/chatbot/playground/${uuidv4()}`}
-                  variant="inline-link"
-                >
-                  New session
-                </RouterButton>
-                <Button
-                  iconAlt="Refresh list"
-                  iconName="refresh"
-                  variant="inline-link"
-                  onClick={() => getSessions()}
-                >
-                  Refresh
-                </Button>
-                <Button
-                  iconAlt="Delete all sessions"
-                  iconName="delete-marker"
-                  variant="inline-link"
-                  onClick={() => deleteUserSessions()}
-                >
-                  Delete all sessions
-                </Button>
-              </SpaceBetween>
-            </div>
-          </div>
-        }
-        columnDefinitions={
-          [
-            {
-              id: "title",
-              header: "Title",
-              sortingField: "title",
-              cell: (e) => (
-                <Link to={`/chatbot/playground/${e.id}`}>{e.title}</Link>
-              ),
-              isRowHeader: true,
+    <Table
+      {...collectionProps}
+      variant="full-page"
+      items={items}
+      pagination={<Pagination {...paginationProps} />}
+      loadingText="Loading history"
+      loading={isLoading}
+      resizableColumns
+      header={
+        <Header
+          variant="awsui-h1-sticky"
+          actions={
+            <SpaceBetween direction="horizontal" size="m" alignItems="center">
+              <RouterButton
+                iconName="add-plus"
+                href={`/chatbot/playground/${uuidv4()}`}
+                variant="inline-link"
+              >
+                New session
+              </RouterButton>
+              <Button
+                iconAlt="Refresh list"
+                iconName="refresh"
+                variant="inline-link"
+                onClick={() => getSessions()}
+              >
+                Refresh
+              </Button>
+              <Button
+                iconAlt="Delete all sessions"
+                iconName="delete-marker"
+                variant="inline-link"
+                onClick={() => deleteUserSessions()}
+              >
+                Delete all sessions
+              </Button>
+            </SpaceBetween>
+          }
+        >
+          History
+        </Header>
+      }
+      columnDefinitions={
+        [
+          {
+            id: "title",
+            header: "Title",
+            sortingField: "title",
+            width: 800,
+            minWidth: 200,
+            cell: (e) => (
+              <Link to={`/chatbot/playground/${e.id}`}>{e.title}</Link>
+            ),
+            isRowHeader: true,
+          },
+          {
+            id: "startTime",
+            header: "Time",
+            sortingField: "startTime",
+            cell: (e: Session) =>
+              DateTime.fromISO(
+                new Date(e.startTime).toISOString()
+              ).toLocaleString(DateTime.DATETIME_SHORT),
+            sortingComparator: (a, b) => {
+              return (
+                new Date(b.startTime).getTime() -
+                new Date(a.startTime).getTime()
+              );
             },
-            {
-              id: "startTime",
-              header: "Time",
-              sortingField: "startTime",
-              cell: (e: Session) =>
-                DateTime.fromISO(
-                  new Date(e.startTime).toISOString()
-                ).toLocaleString(DateTime.DATETIME_SHORT),
-              sortingComparator: (a, b) => {
-                return (
-                  new Date(b.startTime).getTime() -
-                  new Date(a.startTime).getTime()
-                );
-              },
-            },
-            {
-              id: "open",
-              header: "Open",
-              cell: (item) => (
-                <RouterButton
-                  variant="inline-link"
-                  href={`/chatbot/playground/${item.id}`}
-                >
-                  Open
-                </RouterButton>
-              ),
-            },
-            {
-              id: "delete",
-              header: "Delete",
-              cell: (item) => (
-                <Button
-                  variant="inline-link"
-                  onClick={() => deleteSession(item.id)}
-                >
-                  Delete
-                </Button>
-              ),
-            },
-          ] as TableProps.ColumnDefinition<Session>[]
-        }
-      />
-    </div>
+          },
+          {
+            id: "delete",
+            width: 20,
+            header: "",
+            cell: (item) => (
+              <Button
+                variant="inline-icon"
+                iconName="remove"
+                onClick={() => deleteSession(item.id)}
+              >
+                Delete
+              </Button>
+            ),
+          },
+        ] as TableProps.ColumnDefinition<Session>[]
+      }
+    />
   );
 }
