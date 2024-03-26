ae897c6435246bad4e825b01fdd9876be2ae7c7c
Massimiliano Angelino <angmas@amazon.com>
2024-03-26 18:31:19 +0100

    feat(sessions): help panel

--- lib/user-interface/react-app/src/pages/chatbot/playground/playground.tsx
+++ lib/user-interface/react-app/src/pages/chatbot/playground/playground.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import BaseAppLayout from "../../../components/base-app-layout";
 import Chat from "../../../components/chatbot/chat";
-import Sessions from "../../../components/chatbot/sessions";
-import { useParams } from "react-router-dom";
+
+import { Link, useParams } from "react-router-dom";
+import { Header, HelpPanel } from "@cloudscape-design/components";
 
 export default function Playground() {
   const { sessionId } = useParams();
@@ -13,8 +14,18 @@ export default function Playground() {
       toolsHide={false}
       toolsOpen={toolsOpen}
       onToolsChange={({ detail }) => setToolsOpen(detail.open)}
-      tools={<Sessions toolsOpen={toolsOpen} />}
-      toolsWidth={500}
+      tools={
+        <HelpPanel header={<Header variant="h3">Using the chat</Header>}>
+          <div>
+            This chat playground allows user to interact with a chosen LLM and
+            optional RAG retriever. If you select a multimodal model, you can
+            also upload images to use in the conversation. All conversations are
+            saved and can be later accessed via the{" "}
+            <Link to="/chatbot/sessions">Session</Link> in the navigation bar.
+          </div>
+        </HelpPanel>
+      }
+      toolsWidth={300}
       content={<Chat sessionId={sessionId} />}
     />
   );
