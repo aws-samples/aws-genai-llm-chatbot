8a9ce58969251fd2cdd93ee04c371f6ef7611142
Massimiliano Angelino <angmas@amazon.com>
2024-03-27 15:33:21 +0100

    feat(session): one click confirm

--- lib/user-interface/react-app/src/components/chatbot/sessions.tsx
+++ lib/user-interface/react-app/src/components/chatbot/sessions.tsx
@@ -7,6 +7,7 @@ import {
   TableProps,
   Header,
   CollectionPreferences,
+  Modal,
 } from "@cloudscape-design/components";
 import { DateTime } from "luxon";
 import { useState, useEffect, useContext, useCallback } from "react";
@@ -28,6 +29,7 @@ export default function Sessions(props: SessionsProps) {
   const [isLoading, setIsLoading] = useState(true);
   const [selectedItems, setSelectedItems] = useState<Session[]>([]);
   const [preferences, setPreferences] = useState({ pageSize: 20 });
+  const [showModalDelete, setShowModalDelete] = useState(false);
 
   const { items, collectionProps, paginationProps } = useCollection(sessions, {
     filtering: {
@@ -99,129 +101,157 @@ export default function Sessions(props: SessionsProps) {
   };
 
   return (
-    <Table
-      {...collectionProps}
-      variant="full-page"
-      items={items}
-      onSelectionChange={({ detail }) => {
-        console.log(detail);
-        setSelectedItems(detail.selectedItems);
-      }}
-      selectedItems={selectedItems}
-      selectionType="multi"
-      trackBy="id"
-      empty={
-        <Box margin={{ vertical: "xs" }} textAlign="center" color="inherit">
-          <SpaceBetween size="m">
-            <b>No sessions</b>
-          </SpaceBetween>
-        </Box>
-      }
-      ariaLabels={{
-        selectionGroupLabel: "Items selection",
-        allItemsSelectionLabel: ({ selectedItems }) =>
-          `${selectedItems.length} ${
-            selectedItems.length === 1 ? "item" : "items"
-          } selected`,
-        itemSelectionLabel: ({ selectedItems }, item) => item.title!, // eslint-disable-line @typescript-eslint/no-unused-vars
-      }}
-      pagination={<Pagination {...paginationProps} />}
-      loadingText="Loading history"
-      loading={isLoading}
-      resizableColumns
-      stickyHeader={true}
-      preferences={
-        <CollectionPreferences
-          onConfirm={({ detail }) =>
-            setPreferences({ pageSize: detail.pageSize ?? 20 })
-          }
-          title="Preferences"
-          confirmLabel="Confirm"
-          cancelLabel="Cancel"
-          preferences={preferences}
-          pageSizePreference={{
-            title: "Page size",
-            options: [
-              { value: 10, label: "10" },
-              { value: 20, label: "20" },
-              { value: 50, label: "50" },
-            ],
-          }}
-        />
-      }
-      header={
-        <Header
-          description="List of past sessions"
-          variant="awsui-h1-sticky"
-          actions={
-            <SpaceBetween direction="horizontal" size="m" alignItems="center">
-              <RouterButton
-                iconName="add-plus"
-                href={`/chatbot/playground/${uuidv4()}`}
-                variant="inline-link"
-              >
-                New session
-              </RouterButton>
-              <Button
-                iconAlt="Refresh list"
-                iconName="refresh"
-                variant="inline-link"
-                onClick={() => getSessions()}
-              >
-                Refresh
+    <>
+      <Modal
+        onDismiss={() => setShowModalDelete(false)}
+        visible={showModalDelete}
+        footer={
+          <Box float="right">
+            <SpaceBetween direction="horizontal" size="xs">
+              {" "}
+              <Button variant="link" onClick={() => setShowModalDelete(false)}>
+                Cancel
               </Button>
-              <Button
-                iconAlt="Delete"
-                iconName="remove"
-                variant="inline-link"
-                onClick={() => deleteSelectedSessions()}
-              >
-                Delete
+              <Button variant="primary" onClick={deleteSelectedSessions}>
+                Ok
               </Button>
-              <Button
-                iconAlt="Delete all sessions"
-                iconName="delete-marker"
-                variant="inline-link"
-                onClick={() => deleteUserSessions()}
-              >
-                Delete all sessions
-              </Button>
+            </SpaceBetween>{" "}
+          </Box>
+        }
+        header={"Delete session" + (selectedItems.length > 1 ? "s" : "")}
+      >
+        Do you want to delete{" "}
+        {selectedItems.length == 1
+          ? `session ${selectedItems[0].id}?`
+          : `${selectedItems.length} sessions?`}
+      </Modal>
+      <Table
+        {...collectionProps}
+        variant="full-page"
+        items={items}
+        onSelectionChange={({ detail }) => {
+          console.log(detail);
+          setSelectedItems(detail.selectedItems);
+        }}
+        selectedItems={selectedItems}
+        selectionType="multi"
+        trackBy="id"
+        empty={
+          <Box margin={{ vertical: "xs" }} textAlign="center" color="inherit">
+            <SpaceBetween size="m">
+              <b>No sessions</b>
             </SpaceBetween>
-          }
-        >
-          Session History
-        </Header>
-      }
-      columnDefinitions={
-        [
-          {
-            id: "title",
-            header: "Title",
-            sortingField: "title",
-            width: 800,
-            minWidth: 200,
-            cell: (e) => (
-              <Link to={`/chatbot/playground/${e.id}`}>{e.title}</Link>
-            ),
-            isRowHeader: true,
-          },
-          {
-            id: "startTime",
-            header: "Time",
-            sortingField: "startTime",
-            cell: (e: Session) =>
-              DateTime.fromISO(
-                new Date(e.startTime).toISOString()
-              ).toLocaleString(DateTime.DATETIME_SHORT),
-            sortingComparator: (a, b) => {
-              return (
-                new Date(b.startTime).getTime() -
-                new Date(a.startTime).getTime()
-              );
+          </Box>
+        }
+        ariaLabels={{
+          selectionGroupLabel: "Items selection",
+          allItemsSelectionLabel: ({ selectedItems }) =>
+            `${selectedItems.length} ${
+              selectedItems.length === 1 ? "item" : "items"
+            } selected`,
+          itemSelectionLabel: ({ selectedItems }, item) => item.title!, // eslint-disable-line @typescript-eslint/no-unused-vars
+        }}
+        pagination={<Pagination {...paginationProps} />}
+        loadingText="Loading history"
+        loading={isLoading}
+        resizableColumns
+        stickyHeader={true}
+        preferences={
+          <CollectionPreferences
+            onConfirm={({ detail }) =>
+              setPreferences({ pageSize: detail.pageSize ?? 20 })
+            }
+            title="Preferences"
+            confirmLabel="Confirm"
+            cancelLabel="Cancel"
+            preferences={preferences}
+            pageSizePreference={{
+              title: "Page size",
+              options: [
+                { value: 10, label: "10" },
+                { value: 20, label: "20" },
+                { value: 50, label: "50" },
+              ],
+            }}
+          />
+        }
+        header={
+          <Header
+            description="List of past sessions"
+            variant="awsui-h1-sticky"
+            actions={
+              <SpaceBetween direction="horizontal" size="m" alignItems="center">
+                <RouterButton
+                  iconName="add-plus"
+                  href={`/chatbot/playground/${uuidv4()}`}
+                  variant="inline-link"
+                >
+                  New session
+                </RouterButton>
+                <Button
+                  iconAlt="Refresh list"
+                  iconName="refresh"
+                  variant="inline-link"
+                  onClick={() => getSessions()}
+                >
+                  Refresh
+                </Button>
+                <Button
+                  disabled={selectedItems.length == 0}
+                  iconAlt="Delete"
+                  iconName="remove"
+                  variant="inline-link"
+                  onClick={() => {
+                    if (selectedItems.length > 0) setShowModalDelete(true);
+                  }}
+                >
+                  Delete
+                </Button>
+                <Button
+                  iconAlt="Delete all sessions"
+                  iconName="delete-marker"
+                  variant="inline-link"
+                  onClick={() => deleteUserSessions()}
+                >
+                  Delete all sessions
+                </Button>
+              </SpaceBetween>
+            }
+          >
+            Session History
+          </Header>
+        }
+        columnDefinitions={
+          [
+            {
+              id: "title",
+              header: "Title",
+              sortingField: "title",
+              width: 800,
+              minWidth: 200,
+              cell: (e) => (
+                <Link to={`/chatbot/playground/${e.id}`}>{e.title}</Link>
+              ),
+              isRowHeader: true,
             },
-          },
-        ] as TableProps.ColumnDefinition<Session>[]
-      }
-    />
+            {
+              id: "startTime",
+              header: "Time",
+              sortingField: "startTime",
+              cell: (e: Session) =>
+                DateTime.fromISO(
+                  new Date(e.startTime).toISOString()
+                ).toLocaleString(DateTime.DATETIME_SHORT),
+              sortingComparator: (a, b) => {
+                return (
+                  new Date(b.startTime).getTime() -
+                  new Date(a.startTime).getTime()
+                );
+              },
+            },
+          ] as TableProps.ColumnDefinition<Session>[]
+        }
+      />
+    </>
   );
 }
