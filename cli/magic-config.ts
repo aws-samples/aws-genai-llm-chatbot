2f068d4e8eeb4a7dad5c292301242877d95792c9
Charles Marion <chmrion@amazon.com>
2024-08-15 19:06:52 +0000

    bug: Add prefix validation to prevent deloyment failure.

--- cli/magic-config.ts
+++ cli/magic-config.ts
@@ -256,6 +256,12 @@ async function processCreateOptions(options: any): Promise<void> {
       message: "Prefix to differentiate this deployment",
       initial: options.prefix,
       askAnswered: false,
+      validate(value: string) {
+        const regex = /^[a-zA-Z0-9-]{0,10}$/;
+        return regex.test(value)
+          ? true
+          : "Only letters, number and dashes are allowed. The max length is 10 characters.";
+      },
     },
     {
       type: "confirm",
